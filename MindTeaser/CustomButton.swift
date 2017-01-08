fef41f4d36bbe076101079f7271c31b49e6404c9
karol-pilch <karoop@gmail.com>
2017-01-08 13:34:32 +0000

    Finished lesson 5: Added Facebook POP to the button.

--- MindTeaser/CustomButton.swift
+++ MindTeaser/CustomButton.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import pop
 
 @IBDesignable
 class CustomButton: UIButton {
@@ -39,7 +40,42 @@ class CustomButton: UIButton {
 	
 	// Applies custom metrics to the view.
 	private func setupView() {
+		// 1. Apply custom metrics:
 		self.tintColor = fontColor
 		self.layer.cornerRadius = cornerRadius
+		
+		// 2. Register animation for events:
+		
+		// Make small when touch enters:
+		self.addTarget(self, action: #selector(CustomButton.scaleToSmall), for: .touchDown)
+		self.addTarget(self, action: #selector(CustomButton.scaleToSmall), for: .touchDragEnter)
+		
+		// Spring when touch ends:
+		self.addTarget(self, action: #selector(CustomButton.scaleAnimation), for: .touchUpInside)
+		
+		// Make normal when touch leaves without ending:
+		self.addTarget(self, action: #selector(CustomButton.scaleDefault), for: .touchDragExit)
 	}
+	
+	/***** POP *****/
+	func scaleToSmall() {
+		let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+		scaleAnim?.toValue = CGSize(width: 0.95, height: 0.95)
+		self.layer.pop_add(scaleAnim, forKey: "layerScaleSmallAnimation")
+	}
+	
+	func scaleAnimation() {
+		let scaleAnim = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)
+		scaleAnim?.velocity = CGSize(width: 3.0, height: 3.0)
+		scaleAnim?.toValue = CGSize(width: 1.0, height: 1.0)
+		scaleAnim?.springBounciness = 18
+		self.layer.pop_add(scaleAnim, forKey: "layerScaleSprintAnimation")
+	}
+	
+	func scaleDefault() {
+		let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+		scaleAnim?.toValue = CGSize(width: 1.0, height: 1.0)
+		self.layer.pop_add(scaleAnim, forKey: "layerScaleDefaultAnimation")
+	}
+	
 }
